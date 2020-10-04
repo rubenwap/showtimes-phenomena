37b91cfff2165ddb8f5798b5a244625c97d9d4da
Ruben Sanchez <rubensanchezgarcia@gmail.com>
2020-10-04 22:37:52 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/rubenwap/scraper-phenomena
+
+go 1.12
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	github.com/antchfx/xmlquery v1.3.2 // indirect
+	github.com/coreos/go-systemd v0.0.0-20190719114852-fd7a80b32e1f // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0
+	github.com/google/uuid v1.1.2
+	github.com/jackc/pgx/v4 v4.9.0
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/kr/pty v1.1.8 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/temoto/robotstxt v1.1.1 // indirect
+	golang.org/x/net v0.0.0-20201002202402-0a1ea396d57c // indirect
+	golang.org/x/tools v0.0.0-20191029190741-b9c20aec41a5 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"github.com/gocolly/colly"
+	"strings"
+	"encoding/json"
+)
+
+// Movie holds the movie object where the scraped films will be stored
+type Movie struct {
+	Title   string
+	Details string
+	Theatre string
+	Date    time.Time
+}
+
+func main() {
+
+daysTranslate := strings.NewReplacer(
+    "Lunes", "Monday",
+    "Martes", "Tuesday",
+	"Miercoles", "Wednesday",
+	"Miércoles", "Wednesday",
+    "Jueves", "Thursday",
+    "Viernes", "Friday",
+    "Sabado", "Saturday",
+    "Sábado", "Saturday",
+    "Domingo", "Sunday",
+)
+
+	movies := []Movie{}
+	c := colly.NewCollector()
+	detailCollector := c.Clone()
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnHTML(".event-titulo", func(e *colly.HTMLElement) {
+		events := e.ChildAttrs("a", "href")
+		event := events[len(events)-1]
+		detailCollector.Visit("http://www.phenomena-experience.com/" + event)
+	})
+
+	detailCollector.OnHTML("body", func(e *colly.HTMLElement) {
+
+		if e.DOM.Find(".pase-hora").Length() > 0 {
+			
+			dateStr := daysTranslate.Replace(e.DOM.Find(".pase-fecha").First().Text())
+			dateTime := strings.Fields(e.DOM.Find(".pase-hora").First().Text())[0]
+			movie := Movie{}
+			movie.Title = e.DOM.Find(".titulo").Find("span").Text()
+			movie.Details = e.ChildText(".datos2")
+			movie.Theatre = "Phenomena Experience"
+
+			if t, err := time.Parse("Monday, 02/01/2006 15:04h", fmt.Sprintf("%s %s", dateStr, dateTime)); err != nil {
+				fmt.Printf("Error: %s\n", err)				
+			} else {
+				movie.Date = t.Add(-time.Hour * 2)
+			}
+			movies = append(movies, movie)
+		}
+	})
+	c.OnScraped(func(r *colly.Response) {
+		if data, err := json.Marshal(movies); err != nil {
+            fmt.Println(err)
+        } else {
+            fmt.Println(string(data))
+        }
+		})
+
+	c.Visit("http://www.phenomena-experience.com/programacion-mensual/todo.html")
+}
